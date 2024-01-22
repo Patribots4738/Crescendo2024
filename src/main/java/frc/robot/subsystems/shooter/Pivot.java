3dfb9111900385e0f5c5689b9351623ff217920f
Jacob Hotz <lordgaming1010@gmail.com>
2024-01-22 14:13:26 -0800

    Edit Constants
    (this will finish adding all changes currently on origin for all subsystem branches

--- src/main/java/frc/robot/subsystems/shooter/Pivot.java
+++ src/main/java/frc/robot/subsystems/shooter/Pivot.java
@@ -1,40 +1,29 @@
-package frc.robot.subsystems.shooter;
+package frc.robot.subsystems.Shooter;
 
-import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
+public class Pivot {
+    pivot = new Neo(ShooterConstants.SHOOTER_PIVOT_CAN_ID);
+    configMotor();
+}
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.Neo;
-import frc.robot.util.Constants.ShooterConstants;
-
-public class Pivot extends SubsystemBase {
-    private Neo pivot;
-    
-    public Pivot() {
-        this.pivot = new Neo(ShooterConstants.SHOOTER_PIVOT_CAN_ID);
-
-        configMotor();
-    }
-
-
-    public void configMotor() {
-        pivot.setSmartCurrentLimit(ShooterConstants.PIVOT_CURRENT_LIMIT);
+public void configMotor() {
+        pivot.setSmartCurrentLimit(PivotConstants.PIVOT_CURRENT_LIMIT);
+        // See https://docs.revrobotics.com/sparkmax/operating-modes/control-interfaces
         pivot.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 65535);
         pivot.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 65535);
         pivot.setInverted(false);
 
-        pivot.setPID(
-                ShooterConstants.PIVOT_P,
-                ShooterConstants.PIVOT_I,
-                ShooterConstants.PIVOT_D,
-                ShooterConstants.PIVOT_MIN_OUTPUT,
-                ShooterConstants.PIVOT_MAX_OUTPUT);
+        Pivot.setPID(
+                ShooterConstants.SHOOTER_P,
+                ShooterConstants.SHOOTER_I,
+                ShooterConstants.SHOOTER_D,
+                ShooterConstants.SHOOTER_MIN_OUTPUT,
+                ShooterConstants.SHOOTER_MAX_OUTPUT);
 
 
         //sets brake mode
         pivot.setBrakeMode();
     }
     
-    public void setAngle(double angle) {
-        pivot.setTargetPosition(angle);
-    }
+public void setAngle(double angle) {
+    pivot.setTargetPosition(angle);
 }
