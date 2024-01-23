41ee70004eeacc86951db3507f98245489ee2a08
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2024-01-23 15:42:12 -0800

    start up motors when shooting

--- src/main/java/frc/robot/commands/handoff/Handoff.java
+++ src/main/java/frc/robot/commands/handoff/Handoff.java
@@ -4,9 +4,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.Intake;
-import frc.robot.commands.ElevatorCommand;
 import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Swerve;
+import frc.robot.commands.ShooterCalc;
 import frc.robot.subsystems.ElevatorSubs.Claw;
 import frc.robot.subsystems.ElevatorSubs.Elevator;
 import frc.robot.subsystems.shooter.Pivot;
@@ -14,7 +15,7 @@ import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.util.Constants.TrapConstants;
 
 public class Handoff {
-    private final Command emptyCommand = Commands.runOnce(() -> System.out.println(""));
+    private final Command emptyCommand = null;
     private NotePosition notePosition = NotePosition.NONE;
 
     private Intake intake;
@@ -28,13 +29,19 @@ public class Handoff {
     private Pivot pivot;
     private Shooter shooter;
 
+    private ShooterCalc shooterCalc;
+
+    private Swerve swerve;
+
     public Handoff(
             Intake intake, 
             Indexer indexer, 
             Elevator elevator, 
             Claw claw, 
             Pivot pivot, 
-            Shooter shooter) 
+            Shooter shooter,
+            ShooterCalc shooterCalc,
+            Swerve swerve) 
         {
         this.intake = intake;
         this.indexer = indexer;
@@ -42,25 +49,36 @@ public class Handoff {
         this.claw = claw;
         this.pivot = pivot;
         this.shooter = shooter;
+        this.shooterCalc = shooterCalc;
+        this.swerve = swerve;
     }
 
     public Trigger readyToShoot() {
         return new Trigger(() -> pivot.atDesiredAngle() && shooter.atDesiredRPM());
     }
 
+    public Command stopAllMotors() {
+        return Commands.parallel(
+            intake.stop(),
+            indexer.stop(),
+            elevator.stop(),
+            claw.stop(),
+            shooter.stop()
+        );
+    }
+
     // TODO: We assume that the note is already in the claw when we want to shoot
     public Command clawToShooter(){
         // this.notePosition = NotePosition.CLAW; ^^^
         Command shoot = emptyCommand;
-        //TODO: impliment a way to tell the pivot and shooter to go to the correct angles etc... (before checking if ready to shoot)
+        this.shooterCalc.prepareFireCommand(() -> true, swerve.getPose());
         if ( this.readyToShoot().getAsBoolean() ) {
             // run the indexer and intake to make sure the note gets to the shooter
             shoot = 
                 indexer.toShooter()
                     .andThen(intake.inCommand()) // TODO: is this correct? because the top and bottom motors are seperate
-                    .andThen(claw.expel()).andThen(
-                        //TODO: STOP MOTORS
-                    );
+                    .andThen(claw.expel())
+                    .andThen(stopAllMotors());
 
             this.notePosition = NotePosition.SHOOTER;
             //TODO: Then once we are done shooting, we want the variable to be set to NONE
@@ -73,38 +91,19 @@ public class Handoff {
         Command shoot = emptyCommand;
 
         if ( this.notePosition == NotePosition.CLAW ) {
-            // maybe make setPosition a command
-
-            //TODO: find if there is a better way to do this
+            // maybe make setPosition a command ORR Make the Elevator Command 
             shoot = Commands.runOnce(
                         () -> elevator.setPosition(TrapConstants.TRAP_POS)
             ).andThen(
                 Commands.waitUntil(elevator.isAtTargetPosition())
-            ).andThen(
-                claw.expel()
-            ).andThen(new WaitCommand(1)).andThen(//TODO: STOP MOTORS 
-            );
+            ).andThen(claw.expel())
+            .andThen(new WaitCommand(1))
+            .andThen(stopAllMotors());
 
             this.notePosition = NotePosition.CLAW;
 
         }
         return shoot;
     }
-
-    // public void triggerReady() {
-    //     trigger.setIsReady(intake.hasGamePieceTrigger());
-    // }
-
-    // public Trigger readyToShoot() {
-    //     return new Trigger(trigger.hasPiece());
-    // }
-
-    // public Command triggerToShooter() {
-    //     return Commands.either(trigger.toShooter(), emptyCommand, this.readyToShoot());
-    // }
-
-    // public Command triggerToElevator() {
-    //     return Commands.either(trigger.toTrap(), emptyCommand, this.readyToShoot());
-    // }
 
 }
