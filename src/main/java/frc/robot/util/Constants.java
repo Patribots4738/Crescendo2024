46882805af5e5215e3ba83acee7cb4af94e1a8fa
Oliver M. Cushman <olivermcushman@gmail.com>
2024-01-22 22:31:24 -0800

    added pivot max angle const
    this is so we can convert angle in degrees into a position for the pivot motor

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -117,6 +117,8 @@ public final class Constants {
                 public static final double PIVOT_MIN_OUTPUT = -1;
                 public static final double PIVOT_MAX_OUTPUT = 1;
 
+                public static final double PIVOT_MAX_ANGLE = 180.0;
+
                 public static final double MEASUREMENT_INTERVAL_FEET = 1.0;
                 /**
                  * The distances are in feet, the speeds are in RPM, and the angles are in
