3a54a5cd022f1240fb1f888f15c34c7ba96a7456
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-01-26 14:49:21 -0800

    remove second motor and... refactor methods to call a setCommand method

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,59 +8,38 @@ import frc.robot.util.Neo;
 import frc.robot.util.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
-    private final Neo topIntake;
-    private final Neo bottomIntake;
+    private final Neo intakeMotor;
 
     public Intake() {
-        topIntake = new Neo(IntakeConstants.TOP_INTAKE_CAN_ID);
-        bottomIntake = new Neo(IntakeConstants.BOTTOM_INTAKE_CAN_ID);
+        intakeMotor = new Neo(IntakeConstants.TOP_INTAKE_CAN_ID);
         configMotors();
     }
 
     public void configMotors() {
-        bottomIntake.setSmartCurrentLimit(IntakeConstants.INTAKE_CURRENT_LIMIT_AMPS);
+        intakeMotor.setSmartCurrentLimit(IntakeConstants.INTAKE_CURRENT_LIMIT_AMPS);
         // See https://docs.revrobotics.com/sparkmax/operating-modes/control-interfaces
-        bottomIntake.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 65535);
-        bottomIntake.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 65535);
-        bottomIntake.setInverted(false); // TODO: is this correct?
+        intakeMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 65535);
+        intakeMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 65535);
+    }
 
-        topIntake.setSmartCurrentLimit(IntakeConstants.INTAKE_CURRENT_LIMIT_AMPS);
-        // See https://docs.revrobotics.com/sparkmax/operating-modes/control-interfaces
-        topIntake.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 65535);
-        topIntake.setPeriodicFramePeriod(PeriodicFrame.kStatus5, 65535);
-        topIntake.setInverted(false); // TODO: is this correct?
-
-        // sets brake mode
-        topIntake.setBrakeMode();
-        bottomIntake.setBrakeMode();
-
-        // TODO: should we slave the motors together?
+    public Command setCommand(double desiredSpeed) {
+        return runOnce(() -> intakeMotor.set(desiredSpeed));
     }
 
     public Command inCommand() {
-        return runOnce(() -> {
-            topIntake.set(IntakeConstants.INTAKE_SPEED);
-            bottomIntake.set(IntakeConstants.INTAKE_SPEED);
-        });
+        return setCommand(IntakeConstants.INTAKE_SPEED);
     }
 
     public Command outCommand() {
-        return runOnce(() -> {
-            topIntake.set(IntakeConstants.OUTTAKE_SPEED);
-            bottomIntake.set(IntakeConstants.OUTTAKE_SPEED);
-        });
+        return setCommand(IntakeConstants.OUTTAKE_SPEED);
     }
 
     public Command stop() {
-        return runOnce(() -> {
-            topIntake.set(IntakeConstants.STOP_SPEED);
-            bottomIntake.set(IntakeConstants.STOP_SPEED);
-        });
+        return setCommand(IntakeConstants.STOP_SPEED);
     }
 
     public Trigger hasGamePieceTrigger() {
-        return new Trigger(() -> topIntake.getOutputCurrent() > IntakeConstants.HAS_PIECE_CURRENT_THRESHOLD
-                || bottomIntake.getOutputCurrent() > IntakeConstants.HAS_PIECE_CURRENT_THRESHOLD);
+        return new Trigger(() -> intakeMotor.getOutputCurrent() > IntakeConstants.HAS_PIECE_CURRENT_THRESHOLD);
     }
 
 }
