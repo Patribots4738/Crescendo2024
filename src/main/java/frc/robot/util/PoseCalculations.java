5797ab26736627329dbc141ec1d980242cc33c50
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-01-31 00:27:55 -0800

    Remove loggable fields, Fix up static nonsense, and simplify return statement

--- src/main/java/frc/robot/util/PoseCalculations.java
+++ src/main/java/frc/robot/util/PoseCalculations.java
@@ -5,22 +5,14 @@ import java.util.Optional;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.util.Constants.ClimbConstants;
 import frc.robot.util.Constants.FieldConstants;
 import monologue.Logged;
-import monologue.Annotations.Log;
 
 public class PoseCalculations implements Logged {
 
-    @Log.NT
-    private Pose3d chainLeft = new Pose3d();
-    @Log.NT
-    private Pose3d chainRight = new Pose3d();
-
-    public Pair<Double, Double> getChainPosition(Pose2d position) {
+    public static Pair<Double, Double> getChainPosition(Pose2d position) {
         Pose2d closestChainPose;
 
         double minDifference = 10000;
@@ -44,35 +36,13 @@ public class PoseCalculations implements Logged {
         Pose2d relativePosition = position.relativeTo(closestChainPose);
 
         if (Math.abs(relativePosition.getTranslation().getX()) > 1) {
-            chainLeft = new Pose3d();
-            chainRight = new Pose3d();
             return Pair.of(0d, 0d);
         }
 
         double leftIntercept = getChainIntercept(relativePosition.getY() + ClimbConstants.DISTANCE_FROM_ORIGIN_METERS);
         double rightIntercept = getChainIntercept(relativePosition.getY() - ClimbConstants.DISTANCE_FROM_ORIGIN_METERS);
 
-        chainLeft = new Pose3d(
-                        0,
-                        0,
-                        leftIntercept - 0.6, // Convert from the chain's height to the climb's height w/ some extra space
-                        new Rotation3d(
-                            0,
-                            0,
-                            0));
-
-        chainRight = new Pose3d(
-                        0,
-                        0,
-                        rightIntercept - 0.6, // Convert from the chain's height to the climb's height w/ some extra space
-                        new Rotation3d(
-                            0,
-                            0,
-                            0));
-
-        return Pair.of(
-            getChainIntercept(relativePosition.getY() + ClimbConstants.DISTANCE_FROM_ORIGIN_METERS), 
-            getChainIntercept(relativePosition.getY() - ClimbConstants.DISTANCE_FROM_ORIGIN_METERS));
+        return Pair.of(leftIntercept, rightIntercept);
     }
 
     /**
@@ -86,7 +56,7 @@ public class PoseCalculations implements Logged {
      * 
      * @return The height of the chain at that location
      */
-    public double getChainIntercept(double x) {
+    public static double getChainIntercept(double x) {
         // The ds here turn the integers into doubles
         // so that integer divion does not occur.
         double calculation = 3d / 10d * Math.pow(x, 2) + 0.725;
