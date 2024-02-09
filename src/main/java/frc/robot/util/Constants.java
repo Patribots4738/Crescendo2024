fd35c94e523b5e3fbfe1576ed414bdc6afd9a825
Oliver M. Cushman <olivermcushman@gmail.com>
2024-02-09 11:39:18 -0800

    refactored pivot class in accordance with MAXSwerveModule

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -102,7 +102,8 @@ public final class Constants {
         public static final int SHOOTER_PIVOT_CAN_ID = 13;
 
         public static final double SHOOTER_VELOCITY_CONVERSION_FACTOR = 1.0;
-        public static final double PIVOT_POSITION_CONVERSION_FACTOR = FieldConstants.IS_SIMULATION ? 1 : 360;
+        // degrees
+        public static final double PIVOT_POSITION_CONVERSION_FACTOR = 360;
 
         public static final double SHOOTER_P = 0.01;
         public static final double SHOOTER_I = 0;
