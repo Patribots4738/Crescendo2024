e16fd3f253b02eec03e6551e3d613bdd12d2722d
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-02-11 00:03:02 -0800

    Update the pivot to be correct in sim and in real real life

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -105,12 +105,12 @@ public final class Constants {
         // degrees
         public static final double PIVOT_POSITION_CONVERSION_FACTOR = 360;
 
-        public static final double SHOOTER_P = 0.3;
+        public static final double SHOOTER_P = .1;
         public static final double SHOOTER_I = 0;
         public static final double SHOOTER_D = 0;
 
         // TODO: tune pid further
-        public static final double PIVOT_P = 0.05;
+        public static final double PIVOT_P = 0.1;
         public static final double PIVOT_I = 0;
         public static final double PIVOT_D = 0.002;
 
