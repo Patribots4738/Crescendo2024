4072014b688a58b35beada3ad4ad67b4af426968
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2024-02-10 23:29:45 -0800

    revert spacing error

--- src/main/java/frc/robot/util/PatriBoxController.java
+++ src/main/java/frc/robot/util/PatriBoxController.java
@@ -4,7 +4,6 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
