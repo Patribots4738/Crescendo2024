e6799f56c6ce3c10cebcc72ee3862311ca881f0a
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-02-23 02:23:07 -0800

    Fix angle modulus error on swerve modules in sim!

--- src/main/java/frc/robot/util/Neo.java
+++ src/main/java/frc/robot/util/Neo.java
@@ -5,6 +5,8 @@ package frc.robot.util;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.REVPhysicsSim;
 import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.util.Constants.FieldConstants;
@@ -238,7 +240,12 @@ public class Neo extends SafeSpark {
      */
     public void tick() {
         if ((FieldConstants.IS_SIMULATION) && controlType == ControlLoopType.POSITION) {
-            setVoltage(getP() * (targetPosition - getPosition()));
+            double error = (targetPosition - getPosition());
+            // Fix swerve modules trying to go the long way around
+            if (canID < 9) {
+                error = MathUtil.angleModulus(error);
+            }
+            setVoltage(getP() * error);
         }
     }
 
