136396993489e73cadd6f9baeea11919bcaa0923
Oliver M. Cushman <olivermcushman@gmail.com>
2024-02-23 16:16:24 -0800

    friend

--- src/main/java/frc/robot/commands/drive/ChasePose.java
+++ src/main/java/frc/robot/commands/drive/ChasePose.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.drive;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -11,7 +11,7 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Swerve;
-import frc.robot.util.Constants.AutoConstants;
+import frc.robot.util.constants.Constants.AutoConstants;
 
 public class ChasePose extends Command {
 
