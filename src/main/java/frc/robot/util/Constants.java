4981a85ab5de2dd8749fbe8adf6f9131a2afd313
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-03-05 20:23:53 -0800

    Orbit and peniunsula has entered the chat 👀

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -852,7 +852,10 @@ public final class Constants {
             put(W3_RED, "W3_RED");
         }};
 
-      public static final Translation2d PODIUM_POSE = new Translation2d(2.9, 3.4);
+        // 12.4ft from the speaker
+        public static final Translation2d PENINSULA_POSE = new Translation2d(3.697, 4.773);
+        // 9.76ft from the speaker
+        public static final Translation2d ORBIT_POSE = new Translation2d(2.884, 6.304);
 
         public static final List<Pose2d> SHOOTING_POSITIONS = new ArrayList<Pose2d>() {{
             Pose2d L_POSE2D = new Pose2d(L_POSE, Rotation2d.fromDegrees(179.61));
