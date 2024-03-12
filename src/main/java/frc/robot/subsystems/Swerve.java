46ab0be22c1afe0a8714f91a8b7f269115ada20a
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-03-11 22:06:55 -0700

    use 6328's stdevs for swerve

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -104,16 +104,16 @@ public class Swerve extends SubsystemBase implements Logged {
             // standard deviations
             // X, Y, theta
             VecBuilder.fill(
-                Units.feetToMeters(0.3), // 6328 uses 0.003 m here
-                Units.feetToMeters(0.3), // 6328 uses 0.003 m here
-                Units.degreesToRadians(3) // 6328 uses 0.0002 rads here
+                0.003, // 6328 uses 0.003 m here
+                0.003, // 6328 uses 0.003 m here
+                0.0002 // 6328 uses 0.0002 rads here
             ),
             // Vision measurement
             // standard deviations
             // X, Y, theta
             VecBuilder.fill(
-                Units.feetToMeters(2.5), 
-                Units.feetToMeters(2.5), 
+                Units.feetToMeters(2.5),
+                Units.feetToMeters(2.5),
                 Units.degreesToRadians(20)
             )
     );
