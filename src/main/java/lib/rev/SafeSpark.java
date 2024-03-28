c84943394faf49f35fd949c7d4a5159584d5289e
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2024-03-27 21:36:20 -0700

    reverted the things!!

--- src/main/java/lib/rev/SafeSpark.java
+++ src/main/java/lib/rev/SafeSpark.java
@@ -14,6 +14,7 @@ import com.revrobotics.SparkPIDController.ArbFFUnits;
 import com.revrobotics.jni.CANSparkMaxJNI;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import com.revrobotics.SparkRelativeEncoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import frc.robot.Constants.FieldConstants;
