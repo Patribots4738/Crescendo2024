8a76b94e3b5f0feb3faab65937dc069091451fdf
Tina H <148731136+derp00monsta@users.noreply.github.com>
2024-03-27 21:42:08 -0700

    you forgot your coat terry, very important readme stuff

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotState;
+import frc.robot.Constants.FieldConstants;
 import frc.robot.Constants.NTConstants;
 import frc.robot.Constants.ShooterConstants;
 import lib.rev.Neo;
