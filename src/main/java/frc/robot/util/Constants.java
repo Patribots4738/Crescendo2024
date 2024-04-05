a65cc396e4b65fa0c0fef19937e671df4c4fe90a
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-04-04 17:04:26 -0700

    add c5-1 non obj auto

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -415,6 +415,7 @@ public final class Constants {
 
         public static final String[] AUTO_NAMES = new String[] {
             "S C5-4 S OBJ",
+            "S C5-1 S",
             "S W3-1 S C2-5 S",
             "S W3-1 S C2-5 S 2",
             "S W3-1 S C2-3 S 2 OBJ",
