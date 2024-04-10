2cc3b0280e68b2229e8432a3580b3cfe6f743ae1
Jacob1010-h <lordgaming1010@gmail.com>
2024-04-10 15:44:27 -0700

    scale3d what the hecc man

--- src/main/java/frc/robot/util/custom/geometry/scale/Scale3d.java
+++ src/main/java/frc/robot/util/custom/geometry/scale/Scale3d.java
@@ -1,8 +1,8 @@
 package frc.robot.util.custom.geometry.scale;
 
 /**
- * This class is used to represent a 3D scale.
- * Primarily, for FMAPs with april tags.
+ * This class is used to represent a 3D scale. Primarily, for FMAPs with april
+ * tags.
  */
 public class Scale3d {
     public double x;
@@ -19,6 +19,10 @@ public class Scale3d {
         this(x, y, 1);
     }
 
+    public Scale3d(double x) {
+        this(x, 1, 1);
+    }
+
     public Scale3d() {
         this(1, 1, 1);
     }
@@ -30,36 +34,39 @@ public class Scale3d {
     public Scale3d of(double x, double y, double z) {
         return new Scale3d(x, y, z);
     }
-    
-    @Override
+
     public Scale3d of(double x, double y) {
         return new Scale3d(x, y);
     }
-    @Override
+
+    public Scale3d of(double x) {
+        return new Scale3d(x);
+    }
+
     public Scale3d of() {
         return new Scale3d();
     }
-    @Override
+
     public Scale3d of(Scale3d scale) {
         return new Scale3d(scale);
     }
-    @Override
+
     public Scale3d add(Scale3d scale) {
         return new Scale3d(x + scale.x, y + scale.y, z + scale.z);
     }
-    @Override
+
     public Scale3d subtract(Scale3d scale) {
         return new Scale3d(x - scale.x, y - scale.y, z - scale.z);
     }
-    @Override
+
     public Scale3d multiply(Scale3d scale) {
         return new Scale3d(x * scale.x, y * scale.y, z * scale.z);
     }
-    @Override
+
     public Scale3d divide(Scale3d scale) {
         return new Scale3d(x / scale.x, y / scale.y, z / scale.z);
     }
-    @Override
+
     public Scale3d negate() {
         return new Scale3d(-x, -y, -z);
     }
