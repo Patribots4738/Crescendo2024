8797e815e3206920fcb0fa7bc97848a545a3a119
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-04-11 16:22:14 -0700

    refine intake speed for double amp

--- src/main/java/frc/robot/commands/managers/PieceControl.java
+++ src/main/java/frc/robot/commands/managers/PieceControl.java
@@ -181,7 +181,7 @@ public class PieceControl implements Logged {
 
     public Command intakeForDoubleAmp() {
         return Commands.sequence(
-            intake.inCommandSlow(.45),
+            intake.inCommandSlow(.675),
             ampper.intakeSlow(),
             indexer.toShooterSlow(),
             Commands.runOnce(this::restartDoubleAmpTimer)
