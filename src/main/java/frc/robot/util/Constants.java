9b7c8921c05cb311097a52714780493ad215856b
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-04-19 16:18:24 -0500

    add wing only auto for practice field purposes

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -433,6 +433,7 @@ public final class Constants {
             "S W3-1 S C1-2 S 2",
 
             "S W3-1 S C3-5 S",
+            "S W3-1 S",
             // "S W3-1 S C2-3 S 2 OBJ",
             // "S W3-1 S C2-3 S OBJ",
             // "S W3-1 C1-3 OBJ",
