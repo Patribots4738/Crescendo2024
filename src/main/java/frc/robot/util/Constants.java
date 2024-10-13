5d278e76ef0c84ba50e3224f0020e00400437dac
PatribotsProgramming <patribotsprogramming4738@gmail.com>
2024-10-12 19:12:06 -0700

    qual day 1 auto changes

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -501,7 +501,7 @@ public final class Constants {
             "S W3-1 S C2-3 S", // HOME AUTO
             "S W3-1 S C1-2 S",
             "S W3-1 S C3-5 S",
-            "S W3-1 S C1-3 S",
+            "S W3-1 S C1-5 S",
             "S W3-1 S",
             "S C1-5 S Over W1"
         };
